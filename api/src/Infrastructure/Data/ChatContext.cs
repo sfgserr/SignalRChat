b8eb10724e6fa8e7e6ad9c2aeacf565c3392e3d2
sfgserr <dead01r@gmail.com>
2023-12-04 16:14:05 +0200

    Add Infrastructure layer

---
+++ api/src/Infrastructure/Data/ChatContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ChatContext : DbContext
+    {
+        public ChatContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            SeedData.Seed(modelBuilder);
+        }
+
+        public DbSet<Message> Messages { get; set; }
+    }
+}
