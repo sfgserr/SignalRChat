c63375e4a57073d854b330579b92c919e4d14140
sfgserr <dead01r@gmail.com>
2024-01-09 14:59:59 +0200

    Remove ssl1.cnf

--- web-client/Program.cs
+++ web-client/Program.cs
@@ -68,7 +68,7 @@ namespace Client
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
 
             app.UseStaticFiles();
