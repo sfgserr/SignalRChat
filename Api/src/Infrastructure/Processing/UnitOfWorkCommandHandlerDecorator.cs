7aacfa65298945eb4d9c32be06172232937e63ef
sfgserr <dead01r@gmail.com>
2024-06-30 12:49:01 +0300

    Add Processing decorators

---
+++ Api/src/Infrastructure/Processing/UnitOfWorkCommandHandlerDecorator.cs
@@ -0,0 +1,22 @@
+﻿using Application.Cqrs.Commands;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Infrastructure.Processing
+{
+    internal class UnitOfWorkCommandHandlerDecorator<T>(IUnitOfWork unitOfWork, ICommandHandler<T> decorated) : 
+        ICommandHandler<T> where T : ICommand
+    {
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+        private readonly ICommandHandler<T> _decorated = decorated;
+
+        public async Task Handle(T command)
+        {
+            IDbContextTransaction transaction = await _unitOfWork.BeginTransactionAsync();
+
+            await _decorated.Handle(command);
+
+            await _unitOfWork.SaveChangesAsync(transaction);
+        }
+    }
+}
---
+++ Api/src/Infrastructure/Processing/ValidationCommandHandlerDecorator.cs
@@ -0,0 +1,28 @@
+﻿using Application.Cqrs.Commands;
+using Application.Exceptions;
+using FluentValidation;
+
+namespace Infrastructure.Processing
+{
+    internal class ValidationCommandHandlerDecorator<T>(List<IValidator<T>> validators, ICommandHandler<T> decorated) : 
+        ICommandHandler<T> where T : ICommand
+    {
+        private readonly List<IValidator<T>> _validators = validators;
+        private readonly ICommandHandler<T> _decorated = decorated;
+
+        public async Task Handle(T command)
+        {
+            var errors = _validators.Select(v => v.Validate(command))
+                .SelectMany(r => r.Errors)
+                .Where(e => e != null)
+                .ToList();
+
+            if (errors.Any())
+            {
+                throw new InvalidCommandException(errors.Select(e => e.ErrorMessage).ToList());
+            }
+
+            await _decorated.Handle(command);
+        }
+    }
+}
