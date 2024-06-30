56438ea78507ad21b057f79955f19e7d176a3e4c
sfgserr <dead01r@gmail.com>
2024-06-30 18:02:41 +0300

    Add UserContext

---
+++ Api/src/Application/Users/UserContext.cs
@@ -0,0 +1,12 @@
+﻿using Application.Contracts;
+using Domain.Users;
+
+namespace Application.Users
+{
+    public class UserContext(IUserService userService) : IUserContext
+    {
+        private readonly IUserService _userService = userService;
+
+        public UserId Id => new(_userService.GetUserId());
+    }
+}
